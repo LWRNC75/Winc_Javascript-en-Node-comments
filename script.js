c553ff608c26deb2f4791dd7cca7016473e7489e
LWRNC75 <lawrence.kartodimedjo@gmail.com>
2020-09-07 22:19:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('Hallo Winc Academy studenten');
+// Deel 2:
+// 3. "Dit is een grote som".
+// 1.
+console.log(1230941 * 1823794);
+// 2.
+console.log(637263 / 54);
+// 4.
+
+// 5.
+/* Ik ben een blok comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+// 6. Het resultaat wordt nog steeds weergegeven omdat ik de sommen aparte comment staan.
